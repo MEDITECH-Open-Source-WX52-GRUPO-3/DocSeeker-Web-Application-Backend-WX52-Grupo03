2ddfcd75ea6452b1742aee19987186db88baf621
Futuri35 <u201924361@upgc.edu.pe>
2023-06-26 21:33:29 -0500

    first commit

---
+++ src/main/java/com/meditech/healthapp/DockSeeker/DockSeekerApplication.java
@@ -0,0 +1,13 @@
+package com.meditech.healthapp.DockSeeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockSeekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockSeekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meditech/healthapp/DockSeeker/DockSeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditech.healthapp.DockSeeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockSeekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
