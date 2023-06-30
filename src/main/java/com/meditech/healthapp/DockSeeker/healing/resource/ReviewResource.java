78e6e05e1c0a65d7eeda50bbfe6b6d2132ebe789
Wilfredo Futuri Illa <u201924361@upc.edu.pe>
2023-06-30 14:41:47 -0500

    Feat: added review Resource and update Review Resource

---
+++ src/main/java/com/meditech/healthapp/DockSeeker/healing/resource/ReviewResource.java
@@ -0,0 +1,19 @@
+package com.meditech.healthapp.DockSeeker.healing.resource;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewResource {
+    private Long id;
+    private String profilePhotoUrl;
+    private String customerName;
+    private String customerReview;
+    private int customerScore;
+    private  int idPatient;
+    private int idDoctor;
+}
---
+++ src/main/java/com/meditech/healthapp/DockSeeker/healing/resource/UpdateReviewResource.java
@@ -0,0 +1,16 @@
+package com.meditech.healthapp.DockSeeker.healing.resource;
+
+import jakarta.validation.constraints.NotNull;
+
+public class UpdateReviewResource {
+
+    private Long id;
+    private String profilePhotoUrl;
+
+    @NotNull(message = "title is required")
+    private String customerName;
+    private String customerReview;
+    private int customerScore;
+    private  int idPatient;
+    private int idDoctor;
+}
